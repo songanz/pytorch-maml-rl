2bb36114cc2713fb5205b2d364ec67e6e00b26cd
Songan Zhang <songanz@umich.edu>
2020-06-19 00:06:42 -0400

    small change in bash for umich hpc

--- bash/highway-eval.sh
+++ bash/highway-eval.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m cProfile test.py --config configs/maml/highway-eval.yaml --output-folder maml-highway-eval --seed 1 --num-workers 8
+~/Documents/Git_repo/pytorch-maml-rl/venv/bin/python -m cProfile test.py --config configs/maml/highway-eval.yaml --output-folder maml-highway-eval --seed 1 --num-workers 8
